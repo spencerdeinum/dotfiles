3475080d6ac8a7219c7f73abe351aac91080a9eb
Spencer Deinum <spencerdeinum@gmail.com>
2016-04-13 14:19:57 -0400

    Retab and fix quoting
    Former-commit-id: 9b5cb3950ee8c5e8ab1166036909e6be803b9b66

--- stow.sh
+++ stow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for file in *; do
-	if [[ -d "$file" ]]; then
-		echo "stow "$file"" && stow "$file"
-	fi
+    if [[ -d "$file" ]]; then
+        echo "stow $file" && stow "$file"
+    fi
 done
